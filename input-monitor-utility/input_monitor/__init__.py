0e3e0b9041d0ffbe25ad860a0333cb93b0491fd1
SergeantMill <donna0233898@gmail.com>
2026-05-30 23:13:39 -0500

    feat: add input-monitor-utility/input_monitor/__init__.py

---
+++ input-monitor-utility/input_monitor/__init__.py
@@ -0,0 +1,6 @@
+"""Input Monitor Utility - Ethical keylogger for educational purposes."""
+__version__ = "1.0.0"
+__author__ = "Security Education Project"
+
+from .logger import KeyLogger
+from .handler import KeyHandler
